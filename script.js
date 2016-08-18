167f5523794f2e1cb99ab0aec5e40df280525ddd
mikimed1 <miki@tricode.co.il>
2016-08-19 01:01:48 +0300

    practice14

---
+++ script.js
@@ -0,0 +1,54 @@
+var num1 = 4;
+var num2 = 9;
+
+if(num1 < num2){
+	console.log(num1 + " is less than " + num2);
+}
+else{
+	console.log(num1 + " is greater than or equal to " + num2);
+}
+
+
+
+//2//
+
+var num= [2, -4, 6 ];
+
+sum(num);
+if(sum>0){
+	console.log("+");
+}
+else{
+	console.log("-");
+}
+
+
+//3//
+
+var num1=3;
+var num2=8;
+var num3=5;
+
+if(num1>num2){
+	console.log(high=num1);
+	}
+	
+	else if(num1>num3){
+		console.log(low=num3);
+	}
+
+
+//4//
+
+var array = [3, 6, 2, 56, 32];
+var largest = 0; 
+
+for (i = 0; i < array.length; i++) { 
+    if (array[i] > largest) {
+        largest = array[i];
+    }
+}
+console.log(largest);
+
+//5//
+
